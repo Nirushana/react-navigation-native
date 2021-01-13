3292a51ac1a758fad200a62262db5d188eaa2801
Nirushana <kavinda.n.t@gmail.com>
2021-01-14 00:21:12 +0530

    New upload

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigation, createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({navigation}) {
+  return(
+    <View>
+      <Text>Home Screen</Text>
+      <Button title="Go to Detail" onPress={() => navigation.navigate('Details')} />
+    </View>
+  );
+}
+function DetailScreen() {
+  return(
+    <View>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+}
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
